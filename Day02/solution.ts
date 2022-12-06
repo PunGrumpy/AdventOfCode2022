cbee2a4174cf6fb7eae48c30917e4250512a8e12
PunGrumpy <noppakorn.pun@gmail.com>
2022-12-06 22:02:46 +0700

    feat(*): add Day 2 of AOC2022

---
+++ Day02/solution.ts
@@ -0,0 +1,49 @@
+// import fs from 'fs'
+const fs = require('fs')
+
+const data = fs.readFileSync('data.txt', 'utf8').toString().trim().split('\r\n')
+
+// First column
+// A: rock, B: paper, C: scissors
+// Second column
+// X: rock, Y: paper, Z: scissors
+
+const part1 = function (data: string[]) {
+  const map: any = {
+    'A X': 4,
+    'A Y': 8,
+    'A Z': 3,
+    'B X': 1,
+    'B Y': 5,
+    'B Z': 9,
+    'C X': 7,
+    'C Y': 2,
+    'C Z': 6
+  }
+
+  const result: any = []
+  data.forEach(index => result.push(map[index]))
+  const sum = result.reduce((a: number, b: number) => a + b, 0)
+  return sum
+}
+console.log(`Part 1: ${part1(data)}`)
+
+const part2 = function (data: string[]) {
+  const map: any = {
+    'A X': 3,
+    'A Y': 4,
+    'A Z': 8,
+    'B X': 1,
+    'B Y': 5,
+    'B Z': 9,
+    'C X': 2,
+    'C Y': 6,
+    'C Z': 7
+  }
+
+  const result: any = []
+  data.forEach(index => result.push(map[index]))
+  const sum = result.reduce((a: number, b: number) => a + b, 0)
+  return sum
+}
+console.log(`Part 2: ${part2(data)}`)
